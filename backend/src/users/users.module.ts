a4e2894c13e0059383eb34e1fedb49f2f58dc237
rgimprovise <rgimprovise@gmail.com>
2025-11-29 00:22:40 +0300

    Fix: Remove circular dependency between AuthModule and UsersModule

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AdminController } from './admin.controller';
-import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [],
   controllers: [UsersController, AdminController],
   providers: [UsersService],
   exports: [UsersService],
